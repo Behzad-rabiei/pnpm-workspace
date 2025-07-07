754c98e1e63f7820f2f6838b4f15cbc7bd775458
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-07-07 17:16:55 +0200

    style: add biome for ci

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -2,14 +2,14 @@ import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-      test: {
-            globals: true,
-            alias: {
-                  "@": resolve(__dirname, "src"),
-            },
-            coverage: {
-                  reporter: ["text", "html", "lcov"],
-                  exclude: ["**/dist/**", "**/node_modules/**"],
-            },
-      },
+	test: {
+		globals: true,
+		alias: {
+			"@": resolve(__dirname, "src"),
+		},
+		coverage: {
+			reporter: ["text", "html", "lcov"],
+			exclude: ["**/dist/**", "**/node_modules/**"],
+		},
+	},
 });
