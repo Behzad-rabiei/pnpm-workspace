dd51c2a738938db1739b5a34bea76cc3388887bb
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-07-08 09:40:35 +0200

    format

--- apps/api/src/foo/foo.moudle.ts
+++ apps/api/src/foo/foo.moudle.ts
@@ -1,6 +1,6 @@
 // apps/api/src/foo/foo.module.ts
-import { Module } from '@nestjs/common'
-import { FooService } from './foo.service'
+import { Module } from '@nestjs/common';
+import { FooService } from './foo.service';
 
 @Module({
   providers: [FooService],
--- apps/workflows/src/hello.service.test.ts
+++ apps/workflows/src/hello.service.test.ts
@@ -1,8 +1,8 @@
-import { describe, expect, it } from 'vitest'
-import { getHello } from './hello.service'
+import { describe, expect, it } from 'vitest';
+import { getHello } from './hello.service';
 
 describe('API getHello', () => {
   it('returns greeting', () => {
-    expect(getHello()).toBe('Hello World')
-  })
-})
+    expect(getHello()).toBe('Hello World');
+  });
+});
