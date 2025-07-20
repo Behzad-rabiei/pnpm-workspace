ada32b5e16093246ff6ce425fa20e3ef078677d3
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-07-20 16:10:25 +0200

    feat(d): d
    d

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello()
+    }
+
+    @Get('/mamad')
+    getHello1(): string {
+        console.log('dsd')
+        return this.appService.getHello()
+    }
 }
