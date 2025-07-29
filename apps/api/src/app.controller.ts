81c8e24e451d13f98cc81cbf860c94cb5db19e91
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-07-29 15:18:06 +0200

    feat(ui): nooo noo
    nooo nood

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -4,15 +4,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-        return this.appService.getHello()
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
 
-    @Get('healthz')
-    getHealth() {
-        return this.appService.getHealth()
-    }
+  @Get('healthz')
+  getHealth() {
+    return this.appService.getHealth();
+  }
 }
