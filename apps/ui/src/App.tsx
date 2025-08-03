5c98f2a592f04ea79f549ad4dad7f4a0e6de20d5
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-08-03 10:37:10 +0200

    feat(ui): add new page to ui

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count zz {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>count behzad {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
